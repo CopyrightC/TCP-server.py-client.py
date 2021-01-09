ad62d4b0d5a6338732aa618385ea9689100a12c0
Anonymous_Programmer <68592417+SATAN01@users.noreply.github.com>
2021-01-09 18:02:09 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,13 +1,43 @@
-#!/usr/bin/env python3
 import socket
-HOST = ''#host ipv4  
-PORT = 65432        
-
+import os
+import sys
+HOST = ''#ipv4 of host  
+PORT = #port here
+idx = socket.gethostname()
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
+    s.send(idx.encode())
+    ifban = s.recv(1024)
+    ifban = ifban.decode()
+    if ifban.startswith("You are not"):
+        print(ifban)
+        sys.exit()
+    usrn = input("Enter your username : ")
+    s.send(f"{usrn} wants to join the chat, would you like to allow (y/n)?".encode())
     while True:
-        x = input("-> ")
-        s.send(x.encode())
-        data = s.recv(1024)
-        data = data.decode()
-        print(repr(data)
+        permissionx = s.recv(1024)
+        permissionx = permissionx.decode()
+        if permissionx == "y":
+            print("Server has allowed your entry!")
+            break
+        else:
+            print("Server has declined your request to join the chat room")
+            quit()
+    s.send(f"{usrn} has connected to chat!".encode())
+    while True:
+        x = input("YOU : ")
+        x = f"{usrn} : {x}"
+        try:
+            s.send(x.encode())
+            data = s.recv(1024)
+            data = data.decode()
+            if data == "You have been banned by the server!":
+                print(data)
+                sys.exit()
+            if data == f"{usrn}.kick":
+                print('You were kicked by the server')
+                sys.exit()
+            else: print(data)
+        except ConnectionResetError:
+            print("Server is offline")
+            quit()
